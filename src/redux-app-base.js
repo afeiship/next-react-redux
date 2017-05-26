d9210a91d31eba051daa8564f0c3d8b9e08c7334
afeiship <1290657123@qq.com>
2017-05-26 22:46:20 +0800

    bugfix

--- src/redux-app-base.js
+++ src/redux-app-base.js
@@ -5,7 +5,7 @@ var COMMAND = require('./const').COMMAND;
 var ReduxAppBase = createReactClass({
   statics:{
     attachEmiterSystem:function(){
-      var emiterSystem = nx.delete(nx.event, 'init');
+      var emiterSystem = nx.delete(nx.event, ['init']);
       nx.mix(ReduxAppBase.prototype, {
         __listeners__:{}
       },emiterSystem);
