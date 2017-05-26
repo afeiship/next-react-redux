ebe7aa6dbb6471288ef7830c2d1b2e7d4c53aef0
afeiship <1290657123@qq.com>
2017-05-26 16:34:40 +0800

    add command feature for ReduxApp

--- src/redux-app-base.js
+++ src/redux-app-base.js
@@ -1,11 +1,24 @@
 var createReactClass = require('create-react-class');
 var nx = require('next-js-core2');
-var ReduxAppBase =  createReactClass({
-  getInitialState:function(){
-    nx.mix(ReduxAppBase,this.props);
+var ReduxAppBase = createReactClass({
+  mixins: [nx.event],
+  getInitialState: function () {
+    var self = this;
+    nx.mix(ReduxAppBase, this.props, {
+      command: function (inName, inData) {
+        self.props.command(inName, inData, self)
+      }
+    });
+    this.componentAttachCommands();
     return null;
   },
-  render:function(){
+  componentAttachCommands: function () {
+    this.init();
+    this.on('__command__', function (_, inArgs) {
+      this.command && this.command(inArgs.name, inArgs.data);
+    });
+  },
+  render: function () {
     return null;
   }
 });
--- src/redux-boot.js
+++ src/redux-boot.js
@@ -1,8 +1,8 @@
-var React= require('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
 var nx = require('next-js-core2');
-var createStore=require('redux').createStore;
-var bindActionCreators=require('redux').bindActionCreators;
+var createStore = require('redux').createStore;
+var bindActionCreators = require('redux').bindActionCreators;
 var applyMiddleware = require('redux').applyMiddleware;
 var ReduxThunk = require('redux-thunk').default;
 var States = require('next-redux-base').states;
@@ -10,63 +10,63 @@ var Actions = require('next-redux-base').actions;
 var Reducers = require('next-redux-base').reducers;
 
 var ReduxBoot = nx.declare({
-  statics:{
-    run:function(inApp,inAppId){
-      return new ReduxBoot(inApp,inAppId);
+  statics: {
+    run: function (inApp, inAppId) {
+      return new ReduxBoot(inApp, inAppId);
     }
   },
-  properties:{
-    root:{
-      set:function(inValue){
+  properties: {
+    root: {
+      set: function (inValue) {
         this._$actions.root(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getRoot(this._store);
       }
     },
-    error:{
-      set:function(inValue){
+    error: {
+      set: function (inValue) {
         this._$actions.error(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getError(this._store);
       }
     },
-    memory:{
-      set:function(inValue){
+    memory: {
+      set: function (inValue) {
         this._$actions.memory(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getMemory(this._store);
       }
     },
-    request:{
-      set:function(inValue){
+    request: {
+      set: function (inValue) {
         this._$actions.request(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getRequest(this._store);
       }
     },
-    local:{
-      set:function(inValue){
+    local: {
+      set: function (inValue) {
         this._$actions.local(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getLocal();
       }
     },
-    session:{
-      set:function(inValue){
+    session: {
+      set: function (inValue) {
         this._$actions.session(inValue);
       },
-      get:function(){
+      get: function () {
         return States.getSession();
       }
     }
   },
-  methods:{
-    init:function(inApp,inAppId){
+  methods: {
+    init: function (inApp, inAppId) {
       this._app = inApp;
       this._store = createStore(
         this.reducers.bind(this),
@@ -77,23 +77,29 @@ var ReduxBoot = nx.declare({
       this.subscribe();
       this.renderTo();
     },
-    reducers:function (inState,inAction) {
+    reducers: function (inState, inAction) {
       var initialState = this._app.initialState();
-      return Reducers( inState || initialState ,inAction);
+      return Reducers(inState || initialState, inAction);
     },
-    subscribe: function() {
+    subscribe: function () {
       this._store.subscribe(this.renderTo.bind(this));
     },
-    renderTo: function() {
-
+    command: function (inName, inData, inContext) {
+      inContext.fire('__command__', {
+        name: inName,
+        data: inData
+      });
+    },
+    renderTo: function () {
       ReactDOM.render(
         React.createElement(this._app, {
           store: this._store,
-          getState:this._store.getState.bind(this),
-          dispatch:this._store.dispatch.bind(this),
-          actions:bindActionCreators(Actions, this._store.dispatch),
-          update: States.getUpdate.bind(this,this._store),
-          $:this
+          getState: this._store.getState.bind(this),
+          dispatch: this._store.dispatch.bind(this),
+          actions: bindActionCreators(Actions, this._store.dispatch),
+          update: States.getUpdate.bind(this, this._store),
+          command: this.command.bind(this),
+          $: this
         }),
         this._container
       );
