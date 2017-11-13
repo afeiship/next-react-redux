41a41a8e3fe3cdf5784ed848b80525d572dd7d3a
afeiship <1290657123@qq.com>
2017-11-13 13:41:42 +0800

    issue: optmize

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  ReduxAppBase: require('./src/redux-app-base').default,
+  ReduxBoot: require('./src/redux-boot').default
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export default {
+  ReduxAppBase: require('./src/redux-app-base').default,
+  ReduxBoot: require('./src/redux-boot').default
+}
